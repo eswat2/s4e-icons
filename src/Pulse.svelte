38ea3548a426577692a67fbfbc8fd8b71a480d96
Richard Hess <eswat2@gmail.com>
2020-01-26 20:39:19 -0800

    add 3 more

---
+++ src/Pulse.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let hex = undefined
+  export let size = 24
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24">
+  <g fill={hex}>
+    <path
+      d="M3 13H5.79L10.1 4.79L11.28 13.75L14.5 9.66L17.83 13H21V15H17L14.67
+      12.67L9.92 18.73L8.94 11.31L7 15H3V13Z" />
+  </g>
+  <path d="M0 0h24v24H0z" fill="none" />
+</svg>
