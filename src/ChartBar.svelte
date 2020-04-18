798e8e72d87720640eb9315b1c5c961346bb0fb5
Richard Hess <eswat2@gmail.com>
2020-04-17 17:27:30 -0700

    add another bunch of random icons

---
+++ src/ChartBar.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let hex = undefined
+  export let size = 24
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24">
+  <g fill={hex}>
+    <path d="M22 21H2V3H4V19H6V10H10V19H12V6H16V19H18V14H22V21Z" />
+  </g>
+  <path d="M0 0h24v24H0z" fill="none" />
+</svg>
---
+++ src/Protocol.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let hex = undefined
+  export let size = 24
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width={size}
+  height={size}
+  viewBox="0 0 24 24">
+  <g fill={hex}>
+    <path
+      d="M18 20H14L18 4H22M16 4H12L8 20H12M2 16.5A2.5 2.5 0 0 0 4.5 19A2.5 2.5 0
+      0 0 7 16.5A2.5 2.5 0 0 0 4.5 14A2.5 2.5 0 0 0 2 16.5M2 9.5A2.5 2.5 0 0 0
+      4.5 12A2.5 2.5 0 0 0 7 9.5A2.5 2.5 0 0 0 4.5 7A2.5 2.5 0 0 0 2 9.5Z" />
+  </g>
+  <path d="M0 0h24v24H0z" fill="none" />
+</svg>
